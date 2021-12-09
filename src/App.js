9b447a76b1af7b7a1d939783ec84dc3ce9463bc7
Shaik-Chapal <70383236+Shaik-Chapal@users.noreply.github.com>
2021-12-09 17:50:25 +0200

    Order status updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import Home from "./components/Home/Home/Home";
 import Login from "./components/Login/Login";
 import Review from "./components/OrderComponents/Review/Review";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Delivered from "./components/Processing/Delivered/Delivered";
+import Delivering from "./components/Processing/Delivering/Delivering";
+import Delivery from "./components/Processing/Delivery/Delivery";
+import Preparing from "./components/Processing/Preparing/Preparing";
+import Recieved from "./components/Processing/Recieved/Recieved";
 import Registration from "./components/Registration/Registration";
 
 function App() {
@@ -55,6 +60,47 @@ function App() {
         element={
           <PrivateRoute>
             <NewMenuFrom />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/displayMenus/:restaurantName" element={<DisplayMenus />} />
+      <Route
+        path="/recieved"
+        element={
+          <PrivateRoute>
+            <Recieved />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/preparing"
+        element={
+          <PrivateRoute>
+            <Preparing />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/readyForDelivery"
+        element={
+          <PrivateRoute>
+            <Delivery />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/delivering"
+        element={
+          <PrivateRoute>
+            <Delivering />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/delivered"
+        element={
+          <PrivateRoute>
+            <Delivered />
           </PrivateRoute>
         }
       />
